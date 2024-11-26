8119af5c18d3b756bf6846d3ad9bf94d02a27a27
idofogel <ido.moshe.fogel@gmail.com>
2024-11-26 17:15:17 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
+import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Backgrnd from './backgrnd';
 import MemHir from './change_hirarchy';
+import Splash from './splash';
+import ThemeContext from './ThemeContext'
+// const ThemeContext = createContext(null);
 function App() {
+  const [theme, setTheme] = useState(1);
   return (
-    <Routes>
-      <Route path="/" element={<Backgrnd />} />
-      <Route path="/change_hir" element={<MemHir />} />
-    </Routes>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Routes>
+        <Route path="/background" element={<Backgrnd />} />
+        <Route path="/change_hir" element={<MemHir />} />
+        <Route path="/" element={<Splash set_theme={setTheme} />} />
+      </Routes>
+    </ThemeContext.Provider>
   );
 }
 
