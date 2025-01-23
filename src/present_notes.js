e654b877089dc7bfa0c51b9cd81b65ba4667ab8c
idofogel <ido.moshe.fogel@gmail.com>
2025-01-23 20:08:49 +0200

    present_notes

--- src/present_notes.js
+++ src/present_notes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import RoundNode from './roundnode';
 const PresentedNodes = (props) => {
     return (props.items.map((item, index) => (
-        <RoundNode key={item.id_num} callMdl={props.callgroup} title={item.name} placex={item.placex} placey={item.placey} />
+        <RoundNode key={item.id_num} source_node={index} changeLevel={props.levelchanger} callMdl={props.callgroup} node_id={item.id} title={item.name} placex={item.placex} placey={item.placey} />
       ))
     );
 }
